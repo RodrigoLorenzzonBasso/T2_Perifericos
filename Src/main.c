ede2d30e5844db5c37695582fdb52ea346427046
RodrigoLorenzzonBasso <rodrigolorenzzonbasso@gmail.com>
2019-09-17 11:17:53 -0300

    Refatorando o codigo

--- Src/main.c
+++ Src/main.c
@@ -1,41 +1,4 @@
 /* USER CODE BEGIN Header */
-/**
-  ******************************************************************************
-  * @file           : main.c
-  * @brief          : Main program body
-  ******************************************************************************
-  ** This notice applies to any and all portions of this file
-  * that are not between comment pairs USER CODE BEGIN and
-  * USER CODE END. Other portions of this file, whether 
-  * inserted by the user or by software development tools
-  * are owned by their respective copyright owners.
-  *
-  * COPYRIGHT(c) 2019 STMicroelectronics
-  *
-  * Redistribution and use in source and binary forms, with or without modification,
-  * are permitted provided that the following conditions are met:
-  *   1. Redistributions of source code must retain the above copyright notice,
-  *      this list of conditions and the following disclaimer.
-  *   2. Redistributions in binary form must reproduce the above copyright notice,
-  *      this list of conditions and the following disclaimer in the documentation
-  *      and/or other materials provided with the distribution.
-  *   3. Neither the name of STMicroelectronics nor the names of its contributors
-  *      may be used to endorse or promote products derived from this software
-  *      without specific prior written permission.
-  *
-  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-  * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
-  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-  * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-  * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-  *
-  ******************************************************************************
-  */
 /* USER CODE END Header */
 
 /* Includes ------------------------------------------------------------------*/
@@ -88,71 +51,33 @@ void SystemClock_Config(void);
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
 
-uint8_t dadoRX[10];
+struct Control{
 
-uint8_t hora;
+  uint8_t hora, minuto, segundo;
+  RTC_TimeTypeDef sTime;
+	RTC_DateTypeDef sDate;
 
-void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
-{
-  hora = (dadoRX[0] - 0x30)*10 + (dadoRX[1] - 0x30);
-	
-	HAL_UART_Receive_IT(&huart1,dadoRX,2);
-}
+  uint8_t dadoRX[10];
+
+  float temperatura, umidade, pressao;
+  int corrente, potenciometro;
+
+  uint8_t vetor_print[30];
+
+}c;
+
+void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart);
+float le_umidade(void);
+void inicializa_vetor_uint8(uint8_t vetor[], int tam);
+void leitura_AD(int tempo);
+void aciona_PWM(void);
+void renderiza_RTC(void);
+void renderiza_sensores(void);
+void configura_hora(void);
+void inicializa_display(void);
 
 //endereco do sensor de pressao 0xBA e 0xBC
-
 // endereco do sensor ::: 0xBE (Write) 0xBF (Read)
-float le_umidade(void)
-{
-	uint8_t dado[2];
-	dado[0] = 0x82;
-	dado[1] = 0;
-	
-	uint16_t H0_rH_x2, H1_rH_x2;
-	
-	int16_t H0_T0_OUT, H1_T0_OUT;
-	
-	int16_t H_OUT;
-	
-	//escrever na memoria do sensor pra dar WAKE UP
-	HAL_I2C_Mem_Write(&hi2c3,0xBE,0x20,I2C_MEMADD_SIZE_8BIT,&dado[0],1,200);
-	
-	//agora seguir roteiro
-	
-	//1 leitura dos registradores das posicoes 0x30 e 0x31
-	HAL_I2C_Mem_Read(&hi2c3,0xBF,0x30,I2C_MEMADD_SIZE_8BIT,&dado[0],1,200);
-	
-	HAL_I2C_Mem_Read(&hi2c3,0xBF,0x31,I2C_MEMADD_SIZE_8BIT,&dado[1],1,200);
-	
-	H0_rH_x2 = dado[0]/2;
-	H1_rH_x2 = dado[1]/2;
-	
-	float Humidity = 0;
-	
-	//3 leitura dos 0x36, 0x37
-	HAL_I2C_Mem_Read(&hi2c3,0xBF,0x36,I2C_MEMADD_SIZE_8BIT,&dado[0],1,50);
-	HAL_I2C_Mem_Read(&hi2c3,0xBF,0x37,I2C_MEMADD_SIZE_8BIT,&dado[1],1,50);
-	
-	H0_T0_OUT = (dado[1] << 8) + dado[0];
-	
-	//4 leitura 0x3a, 0x3b
-	HAL_I2C_Mem_Read(&hi2c3,0xBF,0x3a,I2C_MEMADD_SIZE_8BIT,&dado[0],1,50);
-	HAL_I2C_Mem_Read(&hi2c3,0xBF,0x3b,I2C_MEMADD_SIZE_8BIT,&dado[1],1,50);
-	
-	H1_T0_OUT = (dado[1] << 8) + dado[0];
-	
-	// 5 leitura do 0x28 e 0x29
-	HAL_I2C_Mem_Read(&hi2c3,0xBF,0x28,I2C_MEMADD_SIZE_8BIT,&dado[0],1,50);
-	HAL_I2C_Mem_Read(&hi2c3,0xBF,0x29,I2C_MEMADD_SIZE_8BIT,&dado[1],1,50);
-	
-	H_OUT = (dado[1] << 8) + dado[0];
-	
-	// 6 calcular
-	
-	Humidity = (((H1_rH_x2 - H0_rH_x2) * (H_OUT - H0_T0_OUT))/(H1_T0_OUT - H0_T0_OUT))+H0_rH_x2;
-		
-	return Humidity;
-}
 
 //
 //	9 SDA - PC9
@@ -163,21 +88,28 @@ float le_umidade(void)
 //
 
 /* USER CODE END 0 */
-
-/**
-  * @brief  The application entry point.
-  * @retval int
-  */
 int main(void)
 {
   /* USER CODE BEGIN 1 */
 	
 	TS_StateTypeDef TsState;
 	
-	RTC_TimeTypeDef sTime;
-	RTC_DateTypeDef sDate;
-	
-	float umidade = 0;
+	c.umidade = 0;
+  c.corrente = 0;
+  c.pressao = 0;
+  c.temperatura = 0;
+  c.potenciometro = 0;
+
+  inicializa_vetor_uint8(c.dadoRX, 10);
+  inicializa_vetor_uint8(c.vetor_print, 30);
+
+  c.hora = 18;
+  c.minuto = 30;
+  c.segundo = 0;
+
+  c.sTime.Hours = c.hora;
+  c.sTime.Minutes = c.minuto;
+  c.sTime.Seconds = c.segundo;
 
   /* USER CODE END 1 */
 
@@ -209,27 +141,14 @@ int main(void)
   MX_ADC1_Init();
   /* USER CODE BEGIN 2 */
 	
-	BSP_LCD_Init();
-	BSP_LCD_LayerDefaultInit(LCD_BACKGROUND_LAYER,LCD_FRAME_BUFFER);
-	BSP_LCD_LayerDefaultInit(LCD_FOREGROUND_LAYER,LCD_FRAME_BUFFER);
-	BSP_LCD_SelectLayer(LCD_FOREGROUND_LAYER);
-	BSP_LCD_DisplayOn();
-	BSP_LCD_Clear(LCD_COLOR_WHITE);
-	BSP_LCD_SetTextColor(LCD_COLOR_RED);
-	BSP_LCD_SetFont(&Font16);
-	//BSP_LCD_DisplayStringAtLine(1,(uint8_t*)"TESTE LINHA 1");
-	BSP_TS_Init(240, 320);
+  inicializa_display();
 	
-	
-	sTime.Hours = 18;
-	sTime.Minutes = 30;
-	sTime.Seconds = 0;
-	HAL_RTC_SetDate(&hrtc, &sDate, FORMAT_BIN);
-  HAL_RTC_SetTime(&hrtc, &sTime, FORMAT_BIN);
+	HAL_RTC_SetDate(&hrtc, &c.sDate, FORMAT_BIN);
+  HAL_RTC_SetTime(&hrtc, &c.sTime, FORMAT_BIN);
 
 
 	// armando primeira interrupcao
-	HAL_UART_Receive_IT(&huart1,dadoRX,2);
+	HAL_UART_Receive_IT(&huart1,c.dadoRX,2);
 
   /* USER CODE END 2 */
 
@@ -241,79 +160,11 @@ int main(void)
 
     /* USER CODE BEGIN 3 */
 		
+		renderizacao_RTC();
+		leitura_AD(200);
+    aciona_PWM();
 
-		unsigned char tt[30];
-		
-		umidade = le_umidade();
-		
-		sprintf((char*)tt,"%f",umidade);
-		
-		//BSP_LCD_Clear(LCD_COLOR_WHITE);
-		BSP_LCD_DisplayStringAtLine(12,tt);
-		
-		//HAL_UART_Receive(&huart1,tt,30,1000);
-		//BSP_LCD_DisplayStringAtLine(4,tt);
-		
-		
-		HAL_RTC_GetTime(&hrtc, &sTime, FORMAT_BIN);
-		HAL_RTC_GetDate(&hrtc, &sDate, FORMAT_BIN);
-		
-		unsigned char print_vector[30];
-		
-		sprintf((char*)print_vector,"%02d:%02d:%02d",sTime.Hours,sTime.Minutes,sTime.Seconds);
-    BSP_LCD_DisplayStringAtLine(8,print_vector);
-		
-		int Pot = 0;
-		int Current = 0;
-		
-		HAL_ADC_Start(&hadc1);
-		HAL_ADC_PollForConversion(&hadc1,100);
-		Pot = HAL_ADC_GetValue(&hadc1); //LEITURA DO CANAL 5
-		HAL_ADC_PollForConversion(&hadc1,100);
-		Current = HAL_ADC_GetValue(&hadc1); //LEITURA DO CANAL 13 (na ordem RANK) //Pino PC3
-		HAL_ADC_Stop(&hadc1);
-		
-		sprintf((char*)print_vector,"%04d",Pot);
-		BSP_LCD_DisplayStringAtLine(6,print_vector);
-		
-		if(Pot > 2000 & Pot < 2095)
-		{
-			BSP_LCD_SetFont(&Font12);
-			BSP_LCD_DisplayStringAtLine(2,(uint8_t*)"motor disligado pora");
-			BSP_LCD_SetFont(&Font16);
-		}
-		else if(Pot >= 2095)
-		{
-			sprintf((char*)print_vector,"Motor Direita : %04d",((Pot-2095)*100)/2000);
-			BSP_LCD_SetFont(&Font12);
-			BSP_LCD_DisplayStringAtLine(2,print_vector);
-			BSP_LCD_SetFont(&Font16);
-		}
-		else if(Pot <= 2000)
-		{
-			sprintf((char*)print_vector,"Motor Esquerda : %04d",((2000-Pot)*100)/2000);
-			BSP_LCD_SetFont(&Font12);
-			BSP_LCD_DisplayStringAtLine(2,print_vector);
-			BSP_LCD_SetFont(&Font16);
-		}
-		
-		HAL_Delay(200);
-		
-		
-		
-		/*BSP_TS_GetState(&TsState);
-		if(TsState.TouchDetected)
-		{
-			//sprintf((char*)vetor,"X=%03d, Y=%03d",TsState.X,TsState.Y);
-			//BSP_LCD_DisplayStringAtLine(4,(uint8_t*)vetor);
-			TsState.X=0;
-			TsState.Y=0;
-		}*/
-		
-		//uint8_t vectorNha[4] = "nha\n";
-		//HAL_UART_Transmit(&huart1,vectorNha, 4, 60);
-		
-		//HAL_Delay(30);
+		HAL_Delay(100);
 		
   }
   /* USER CODE END 3 */
@@ -373,6 +224,156 @@ void SystemClock_Config(void)
 }
 
 /* USER CODE BEGIN 4 */
+
+void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
+{
+  c.hora = (c.dadoRX[0] - 0x30)*10 + (c.dadoRX[1] - 0x30);
+	
+	HAL_UART_Receive_IT(&huart1,c.dadoRX,2);
+}
+
+//endereco do sensor de pressao 0xBA e 0xBC
+
+// endereco do sensor ::: 0xBE (Write) 0xBF (Read)
+float le_umidade(void)
+{
+	uint8_t dado[2];
+	dado[0] = 0x82;
+	dado[1] = 0;
+	
+	uint16_t H0_rH_x2, H1_rH_x2;
+	
+	int16_t H0_T0_OUT, H1_T0_OUT;
+	
+	int16_t H_OUT;
+	
+	//escrever na memoria do sensor pra dar WAKE UP
+	HAL_I2C_Mem_Write(&hi2c3,0xBE,0x20,I2C_MEMADD_SIZE_8BIT,&dado[0],1,200);
+	
+	//agora seguir roteiro
+	
+	//1 leitura dos registradores das posicoes 0x30 e 0x31
+	HAL_I2C_Mem_Read(&hi2c3,0xBF,0x30,I2C_MEMADD_SIZE_8BIT,&dado[0],1,200);
+	
+	HAL_I2C_Mem_Read(&hi2c3,0xBF,0x31,I2C_MEMADD_SIZE_8BIT,&dado[1],1,200);
+	
+	H0_rH_x2 = dado[0]/2;
+	H1_rH_x2 = dado[1]/2;
+	
+	float Humidity = 0;
+	
+	//3 leitura dos 0x36, 0x37
+	HAL_I2C_Mem_Read(&hi2c3,0xBF,0x36,I2C_MEMADD_SIZE_8BIT,&dado[0],1,50);
+	HAL_I2C_Mem_Read(&hi2c3,0xBF,0x37,I2C_MEMADD_SIZE_8BIT,&dado[1],1,50);
+	
+	H0_T0_OUT = (dado[1] << 8) + dado[0];
+	
+	//4 leitura 0x3a, 0x3b
+	HAL_I2C_Mem_Read(&hi2c3,0xBF,0x3a,I2C_MEMADD_SIZE_8BIT,&dado[0],1,50);
+	HAL_I2C_Mem_Read(&hi2c3,0xBF,0x3b,I2C_MEMADD_SIZE_8BIT,&dado[1],1,50);
+	
+	H1_T0_OUT = (dado[1] << 8) + dado[0];
+	
+	// 5 leitura do 0x28 e 0x29
+	HAL_I2C_Mem_Read(&hi2c3,0xBF,0x28,I2C_MEMADD_SIZE_8BIT,&dado[0],1,50);
+	HAL_I2C_Mem_Read(&hi2c3,0xBF,0x29,I2C_MEMADD_SIZE_8BIT,&dado[1],1,50);
+	
+	H_OUT = (dado[1] << 8) + dado[0];
+	
+	// 6 calcular
+	
+	Humidity = (((H1_rH_x2 - H0_rH_x2) * (H_OUT - H0_T0_OUT))/(H1_T0_OUT - H0_T0_OUT))+H0_rH_x2;
+		
+	return Humidity;
+}
+
+float le_temperatura(void)
+{
+
+}
+
+void inicializa_vetor_uint8(uint8_t vetor[], int tam)
+{
+  for(int i = 0; i < tam; i++)
+  {
+    vetor[i] = 0;
+  }
+}
+
+void leitura_AD(int tempo)
+{
+  HAL_ADC_Start(&hadc1);
+  HAL_ADC_PollForConversion(&hadc1,tempo/2);
+  c.potenciometro = HAL_ADC_GetValue(&hadc1); //LEITURA DO CANAL 5
+  HAL_ADC_PollForConversion(&hadc1,tempo/2);
+  c.corrente = HAL_ADC_GetValue(&hadc1); //LEITURA DO CANAL 13 (na ordem RANK) //Pino PC3
+  HAL_ADC_Stop(&hadc1);
+}
+
+void aciona_PWM(void)
+{
+  if(c.potenciometro > 2000 & c.potenciometro < 2095)
+  {
+    BSP_LCD_SetFont(&Font12);
+    BSP_LCD_DisplayStringAtLine(2,(uint8_t*)"motor disligado pora");
+  }
+  else if(c.potenciometro >= 2095)
+  {
+    int pwm_percent = ((c.potenciometro-2095)*100)/2000;
+    sprintf((char*)c.vetor_print,"Motor Direita : %04d",pwm_percent);
+    BSP_LCD_SetFont(&Font12);
+    BSP_LCD_DisplayStringAtLine(3,c.vetor_print);
+  }
+  else if(c.potenciometro <= 2000)
+  {
+    int pwm_percent = ((2000-c.potenciometro)*100)/2000;
+    sprintf((char*)c.vetor_print,"Motor Esquerda : %04d", pwm_percent);
+    BSP_LCD_SetFont(&Font12);
+    BSP_LCD_DisplayStringAtLine(4,c.vetor_print);
+  }
+}
+
+void renderiza_RTC(void)
+{
+  HAL_RTC_GetTime(&hrtc, &c.sTime, FORMAT_BIN);
+  HAL_RTC_GetDate(&hrtc, &c.sDate, FORMAT_BIN);
+  
+  BSP_LCD_SetFont(&Font12);
+  sprintf((char*)c.vetor_print,"%02d:%02d:%02d",c.sTime.Hours,c.sTime.Minutes,c.sTime.Seconds);
+  BSP_LCD_DisplayStringAtLine(1,c.vetor_print);
+}
+
+void renderiza_sensores(void)
+{
+  c.umidade = le_umidade();
+  //c.temperatura = le_temperatura();
+
+  BSP_LCD_SetFont(&Font16);
+  sprintf((char*)c.vetor_print,"%2.1f",c.umidade);
+  BSP_LCD_DisplayStringAtLine(5,c.vetor_print);
+
+  /*BSP_LCD_SetFont(&Font16);
+  sprintf((char*)c.vetor_print,"%2.1f",c.temperatura);
+  BSP_LCD_DisplayStringAtLine(6,c.vetor_print);*/
+}
+
+void configura_hora(void)
+{
+
+}
+
+void inicializa_display(void)
+{
+  BSP_LCD_Init();
+	BSP_LCD_LayerDefaultInit(LCD_BACKGROUND_LAYER,LCD_FRAME_BUFFER);
+	BSP_LCD_LayerDefaultInit(LCD_FOREGROUND_LAYER,LCD_FRAME_BUFFER);
+	BSP_LCD_SelectLayer(LCD_FOREGROUND_LAYER);
+	BSP_LCD_DisplayOn();
+	BSP_LCD_Clear(LCD_COLOR_WHITE);
+	BSP_LCD_SetTextColor(LCD_COLOR_RED);
+	BSP_LCD_SetFont(&Font16);
+	BSP_TS_Init(240, 320);
+}
 
 /* USER CODE END 4 */
 
